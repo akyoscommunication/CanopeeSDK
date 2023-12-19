3065bcca46f48c895a49767f21e3736b4ca212cb
akyoscommunication <50996280+akyoscommunication@users.noreply.github.com>
2023-12-19 14:13:37 +0100

    Update SyncUserParser.php

--- Webhook/SyncUserParser.php
+++ Webhook/SyncUserParser.php
@@ -20,7 +20,7 @@ final class SyncUserParser extends AbstractRequestParser
 
     protected function getRequestMatcher(): RequestMatcherInterface
     {
-        $parsed_url = parse_url($this->container->getParameter('api')['endpoint']);
+        $parsed_url = parse_url($this->container->getParameter('host'));
         $host = $parsed_url['host'];
         return new ChainRequestMatcher([
             new HostRequestMatcher($host),
