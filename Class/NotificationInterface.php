713eddace5c65771b20f26fa36b5cac2a50c9f0b
Lilian Carneau <lilian.akyos@gmail.com>
2024-01-24 17:57:36 +0100

    feat: try

---
+++ Class/NotificationInterface.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Akyos\CanopeeSDK\Class;
+
+use Akyos\CanopeeModuleSDK\Class\Post;
+use App\Enum\Notification\Types;
+use Akyos\CanopeeModuleSDK\Translation\CanopeeTranslatableMessage;
+use Akyos\CanopeeModuleSDK\Class\AbstractQueryObject;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+interface NotificationInterface
+{
+}
